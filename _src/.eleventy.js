33a78e8dbf7022c64bab15accccf3d5b3bd9e43f
Eric <eric.wolter@gmx.de>
2020-01-04 20:52:02 +0100

    adds version 01.2020 based on eleventy

---
+++ _src/.eleventy.js
@@ -0,0 +1,37 @@
+const terser = require('terser');
+const postcss = require('postcss');
+const precss = require('precss');
+const cssnano = require('cssnano');
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addWatchTarget('./css');
+  eleventyConfig.addWatchTarget('./js');
+  eleventyConfig.addWatchTarget('./img');
+
+  eleventyConfig.addPassthroughCopy('./img');
+
+  eleventyConfig.addTransform("postcss", async function(content, outputPath) {
+    if(outputPath.endsWith(".css") ) {
+      let lazy = await postcss([precss]).process(content, { from: outputPath, to: outputPath });
+      return lazy.css;
+    }
+
+    return content;
+  });
+
+  // eleventyConfig.addTransform("terser", function(content, outputPath) {
+  //   if(outputPath.endsWith(".js") ) {
+  //     let minified = terser.minify(content);
+  //     return minified.code;
+  //   }
+
+  //   return content;
+  // });
+  
+  return {
+    dir: {
+      output: "../"
+    },
+    templateFormats: ['pug', 'njk']
+  }
+}
